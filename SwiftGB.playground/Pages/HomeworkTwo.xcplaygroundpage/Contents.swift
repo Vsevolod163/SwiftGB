9c3c303e27096939beb860a89ff1969aca0720cf
Vsevolod <vsevolod.lashin@gmail.com>
2023-06-07 21:02:11 +0400

    HomeworkTwo ex 1 added

--- SwiftGB.playground/Pages/HomeworkTwo.xcplaygroundpage/Contents.swift
+++ SwiftGB.playground/Pages/HomeworkTwo.xcplaygroundpage/Contents.swift
@@ -4,7 +4,11 @@
 // процент, третий аргумент — это срок функция возвращает сколько денег получит
 // пользователь по итогу.
 
+func getTotalAmount(sum: Double, percent: Double, time: Int) -> Double {
+    sum + (sum * percent / 100) * Double(time)
+}
 
+print(getTotalAmount(sum: 120, percent: 10, time: 2))
 
 // 2. Создать перечисление, которое содержит 3 вида пиццы, и создать переменные
 // с каждым видом пиццы.
