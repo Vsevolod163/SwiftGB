bd766f22173b005c6414f608e1c9d71a83b73f2c
Vsevolod <vsevolod.lashin@gmail.com>
2023-06-09 19:26:30 +0400

    HomeworkThree ex 1 added

--- SwiftGB.playground/Pages/HomeworkThree.xcplaygroundpage/Contents.swift
+++ SwiftGB.playground/Pages/HomeworkThree.xcplaygroundpage/Contents.swift
@@ -4,3 +4,30 @@
 // сколько заплатит покупатель (необходимо вывести его номер по порядку,
 // чай, который он заказал и стоимость).
 
+
+enum Tea: String {
+    case green
+    case black
+    case red
+    case white
+}
+
+let teaTypes: [Tea: Int] = [
+    .green: 20,
+    .black: 30,
+    .red: 50,
+    .white: 10
+]
+
+let queue: [Tea] = [
+    .green,
+    .white,
+    .red,
+    .red,
+    .black,
+    .green
+]
+
+for (index, tea) in queue.enumerated() {
+    print(index, tea, teaTypes[tea] ?? Tea.self)
+}
