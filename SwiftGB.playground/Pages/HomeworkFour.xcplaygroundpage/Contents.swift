ed9e6315236b7a67f9c264ecb4e29601af06ed30
Vsevolod <vsevolod.lashin@gmail.com>
2023-06-14 23:35:03 +0400

    HomeworkFour ex 2 added

--- SwiftGB.playground/Pages/HomeworkFour.xcplaygroundpage/Contents.swift
+++ SwiftGB.playground/Pages/HomeworkFour.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
 
 // 1. Создать перечисление с видами пиццы (хотя бы 4-5 кейсов).
 
-enum PizzaTypes {
+enum PizzaType {
     case pepperoni
     case margarita
     case mushroom
@@ -14,8 +14,28 @@ enum PizzaTypes {
 // помидоры, сыр). Вид пиццы должен быть вложенным типом для структуры пиццы.
 // Подсказка: добавки лучше также сделать перечислением.
 
+struct Pizza {
+    let cost: Int
+    let type: PizzaType
+    let dough: Dough
+    let additives: [Additive]
+    
+    enum Dough {
+        case fat
+        case thin
+    }
+    
+    enum Additive {
+        case pepperoni
+        case tomato
+        case cheese
+    }
+    
+}
+
 // 3. Создать класс пиццерии, добавить массив с возможными видами пиццы.
 // Переменная с массивом должна быть приватной. Массив задаётся в инициализаторе.
+
 
 // 4. Написать в классе пиццерии функции для добавления нового вида пиццы и для
 // получения всех доступных пицц.
